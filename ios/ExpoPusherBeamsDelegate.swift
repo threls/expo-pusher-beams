e136b02362805920ea6528e080d48b32a7265a54
ghili <gary@threls.com>
2023-04-14 10:22:12 +0200

    initial commit

---
+++ ios/ExpoPusherBeamsDelegate.swift
@@ -0,0 +1,31 @@
+import ExpoModulesCore
+import PushNotifications
+
+public class ExpoPusherBeamsDelegate: ExpoAppDelegateSubscriber {
+    let beamsClient = PushNotifications.shared
+    
+    public func applicationDidBecomeActive(_ application: UIApplication) {
+        // The app has become active.
+    }
+
+    public func applicationWillResignActive(_ application: UIApplication) {
+        // The app is about to become inactive.
+    }
+
+    public func applicationDidEnterBackground(_ application: UIApplication) {
+        // The app is now in the background.
+    }
+
+    public func applicationWillEnterForeground(_ application: UIApplication) {
+        // The app is about to enter the foreground.
+    }
+
+    public func applicationWillTerminate(_ application: UIApplication) {
+        // The app is about to terminate.
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        self.beamsClient.registerDeviceToken(deviceToken)
+    }
+}
+
--- ios/ExpoPusherBeamsModule.swift
+++ ios/ExpoPusherBeamsModule.swift
@@ -2,7 +2,7 @@ import ExpoModulesCore
 
 public class ExpoPusherBeamsModule: Module {
   // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
+  // that describes the module's functionality and behaviour.
   // See https://docs.expo.dev/modules/module-api for more details about available components.
   public func definition() -> ModuleDefinition {
     // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
@@ -11,9 +11,9 @@ public class ExpoPusherBeamsModule: Module {
     Name("ExpoPusherBeams")
 
     // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants([
-      "PI": Double.pi
-    ])
+    // Constants([
+    //   "PI": Double.pi
+    // ])
 
     // Defines event names that the module can send to JavaScript.
     Events("onChange")
