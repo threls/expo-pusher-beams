652de54c25c1428c4aaf5b432d30f1b1a3af55c2
ghili <gary@threls.com>
2023-04-19 09:13:40 +0200

    feat: work on package

--- src/ExpoPusherBeamsModule.web.ts
+++ src/ExpoPusherBeamsModule.web.ts
@@ -1,12 +1,40 @@
 import { EventEmitter } from 'expo-modules-core';
+import * as PusherPushNotifications from '@pusher/push-notifications-web';
 
 const emitter = new EventEmitter({} as any);
 
+let beamsClient: PusherPushNotifications.Client;
+
+const checkBeamsClient = () => {
+  if (!beamsClient)
+    throw new Error(
+      'Pusher Beams Client needs to be initialised first. Try calling setInstanceId(:instanceId)'
+    );
+};
+
+const setInstanceId = (id: string) => {
+  beamsClient = new PusherPushNotifications.Client({ instanceId: id });
+  beamsClient.start();
+};
+
+const clearAllState = () => {
+  checkBeamsClient();
+  beamsClient.clearAllState();
+};
+
+const subscribe = async (interest: string) => {
+  checkBeamsClient();
+  return beamsClient.addDeviceInterest(interest);
+};
+
+const unsubscribe = async (interest: string) => {
+  checkBeamsClient();
+  return beamsClient.removeDeviceInterest(interest);
+};
+
 export default {
-  setInstanceId(id: string) {
-    console.log('Set instance id:', id);
-  },
-  async subscribe(interest: string): Promise<void> {
-    return new Promise<void>((resolve) => resolve());
-  },
+  setInstanceId,
+  subscribe,
+  unsubscribe,
+  clearAllState,
 };
